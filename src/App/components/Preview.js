365306ed293d5aeb821f2817704d8b145c3f7c67
James Duncan <james.duncan@cru.org>
2020-04-20 16:25:40 +0700

    adjust per Ira's recommendations.

--- src/App/components/Preview.js
+++ src/App/components/Preview.js
@@ -2,11 +2,11 @@ import React from 'react';
 import './Preview.scss';
 
 const HIGHLIGHT_VERSE_INDEX = 0;
-const HIGHLIGHT_WORD_INDEX = [0,1,2];
+const HIGHLIGHT_WORD_INDEXES = [0,1,2];
 
 const PreviewVerse = ({ verse, highlightVerse, highlightStyle }) => {
   return verse.split(' ').map((word, index) => {
-    const isHighlighted = highlightVerse && HIGHLIGHT_WORD_INDEX.indexOf(index) !== -1;
+    const isHighlighted = highlightVerse && HIGHLIGHT_WORD_INDEXES.includes(index);
     return (
       <React.Fragment key={index}>
         <div
