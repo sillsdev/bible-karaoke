757fa20b6d13fc194b92052894a9d6911e9b9787
wongpratan <wongpratan@hotmail.com>
2021-02-01 16:37:52 +0700

    Change how to verify the result of .mergeWavFiles function

--- public/cli/lib/commands/ffmpeg.integration.ts
+++ public/cli/lib/commands/ffmpeg.integration.ts
@@ -33,9 +33,5 @@ test('mergeWavFiles smoke test: multiple files: success', async (t) => {
   const skipFiles: string[] = [];
   const newFilePath = await mergeWavFiles(ffmpegPath, wavFiles, skipFiles);
 
-  // it is a wav file who is in the temporary folder.
-  // like '/tmp/323f39912f9f9ff84c29479bfb77a266/bbkAudio.wav'
-  // or '/temp/323f39912f9f9ff84c29479bfb77a266/bbkAudio.wav'
-  const regex = new RegExp(/\/\w+\/\w+\/bbkAudio.wav/g);
-  t.true(regex.test(newFilePath));
+  t.true(fs.existsSync(newFilePath));
 });
