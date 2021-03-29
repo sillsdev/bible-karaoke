63d2de20123e310e9fcf9f89686e64388d4a3d35
wongpratan <wongpratan@hotmail.com>
2021-03-29 15:54:49 +0700

    -Remove the JS files

--- public/sources/index.js
+++
@@ -1,7 +0,0 @@
-const hearThis = require('./hear-this');
-const scriptureAppBuilder = require('./scripture-app-builder');
-
-module.exports = {
-  [hearThis.PROJECT_TYPE]: hearThis,
-  [scriptureAppBuilder.PROJECT_TYPE]: scriptureAppBuilder,
-};
