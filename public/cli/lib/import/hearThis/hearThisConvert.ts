5d225a851d0c2cc5f2d6d2410bdeb3e968616d79
nate <7259@pm.me>
2021-03-29 15:09:21 +0700

    Remove saving json from HT import
    - remove debugging code
    - requested PR changes

--- public/cli/lib/import/hearThis/hearThisConvert.ts
+++ public/cli/lib/import/hearThis/hearThisConvert.ts
@@ -1,9 +1,8 @@
 import fs from 'fs';
 import path from 'path';
 import { xml2json } from 'xml-js';
-import { getIntermediateRootDir, mkDir } from '../import-util';
 import shell from 'shelljs';
-import { BKChapter } from '../../../../models/projectFormat.model';
+import { BKProject, BKChapter } from '../../../../models/projectFormat.model';
 import { ConvertProject, ConvertBook, ConvertChapter } from '../../../../models/convertFormat.model';
 
 //Defines a ScriptLine as expected after parsing the hearthis xml
@@ -20,11 +19,8 @@ async function convertChapter(
   chapter: ConvertChapter,
   book: ConvertBook,
   project: ConvertProject,
-  projectDir: string,
   ffprobePath: string
-): Promise<string | Error> {
-  const chapterDir = path.join(projectDir, book.name, chapter.name);
-  mkDir(chapterDir);
+): Promise<BKChapter> {
   const chapterDetails: BKChapter = {
     book: book.name,
     chapter: chapter.name,
@@ -36,68 +32,56 @@ async function convertChapter(
   };
   const sourceChapterDir = path.join(project.fullPath, book.name, chapter.name);
   const infoXmlPath = path.join(sourceChapterDir, 'info.xml');
-  try {
-    const infoXmlFileContents = fs.readFileSync(infoXmlPath, { encoding: 'utf-8' });
-    const chapterInfo = JSON.parse(xml2json(infoXmlFileContents, { compact: true }));
-    // make sure ScriptLine is an array
-    if (!Array.isArray(chapterInfo.ChapterInfo.Recordings.ScriptLine)) {
-      chapterInfo.ChapterInfo.Recordings.ScriptLine = [chapterInfo.ChapterInfo.Recordings.ScriptLine];
-    }
 
-    let chapterAudioLength = 0;
-    for await (const scriptLine of chapterInfo.ChapterInfo.Recordings.ScriptLine as ScriptLine[]) {
-      // Fix #20 : ignore Chapter Headings
-      if (scriptLine.HeadingType != null && scriptLine.HeadingType._text == 'c') {
-        continue;
-      }
-
-      const segmentId = parseInt(scriptLine.LineNumber._text, 10);
-      const audioPath: string = path.join(sourceChapterDir, `${segmentId - 1}.wav`);
-      const duration = await getAudioDurationInMilliseconds(audioPath, ffprobePath);
-      // if only one audio file then simplify audio property
-      if (chapterInfo.ChapterInfo.Recordings.ScriptLine.length === 1) {
-        chapterDetails.audio = { filename: audioPath, length: duration };
-      } else if ('files' in chapterDetails.audio) {
-        chapterDetails.audio.files.push({ filename: audioPath, length: duration });
-      }
-      chapterDetails.segments.push({
-        segmentId,
-        text: scriptLine.Text._text,
-        verse: scriptLine.Verse._text,
-        startTime: chapterAudioLength,
-        length: duration,
-        isHeading: scriptLine.Heading._text === 'true',
-      });
-      chapterAudioLength += duration;
-      chapterDetails.audio.length = chapterAudioLength;
-    }
-    const jsonFilename = path.join(chapterDir, 'chapter.json');
-    const jsonFileContents = JSON.stringify(chapterDetails, null, 2);
-    try {
-      fs.writeFileSync(jsonFilename, jsonFileContents);
-    } catch (e) {
-      return e;
-    }
-    return jsonFilename;
-  } catch (e) {
-    return e;
+  const infoXmlFileContents = fs.readFileSync(infoXmlPath, { encoding: 'utf-8' });
+  const chapterInfo = JSON.parse(xml2json(infoXmlFileContents, { compact: true }));
+  // make sure ScriptLine is an array
+  if (!Array.isArray(chapterInfo.ChapterInfo.Recordings.ScriptLine)) {
+    chapterInfo.ChapterInfo.Recordings.ScriptLine = [chapterInfo.ChapterInfo.Recordings.ScriptLine];
   }
+
+  let chapterAudioLength = 0;
+  for await (const scriptLine of chapterInfo.ChapterInfo.Recordings.ScriptLine as ScriptLine[]) {
+    // Fix #20 : ignore Chapter Headings
+    if (scriptLine.HeadingType != null && scriptLine.HeadingType._text == 'c') {
+      continue;
+    }
+
+    const segmentId = parseInt(scriptLine.LineNumber._text, 10);
+    const audioPath: string = path.join(sourceChapterDir, `${segmentId - 1}.wav`);
+    const duration = await getAudioDurationInMilliseconds(audioPath, ffprobePath);
+    // if only one audio file then simplify audio property
+    if (chapterInfo.ChapterInfo.Recordings.ScriptLine.length === 1) {
+      chapterDetails.audio = { filename: audioPath, length: duration };
+    } else if ('files' in chapterDetails.audio) {
+      chapterDetails.audio.files.push({ filename: audioPath, length: duration });
+    }
+    chapterDetails.segments.push({
+      segmentId,
+      text: scriptLine.Text._text,
+      verse: scriptLine.Verse._text,
+      startTime: chapterAudioLength,
+      length: duration,
+      isHeading: scriptLine.Heading._text === 'true',
+    });
+    chapterAudioLength += duration;
+    chapterDetails.audio.length = chapterAudioLength;
+  }
+
+  return chapterDetails;
 }
 
-export async function convert(project: ConvertProject, ffprobePath: string): Promise<string | Error> {
-  const projectDir = path.join(getIntermediateRootDir(), project.name);
-  mkDir(projectDir);
+export async function convert(project: ConvertProject, ffprobePath: string): Promise<BKProject> {
+  const bkProject: BKProject = { dirName: project.fullPath, books: [] };
   for await (const book of project.books) {
-    const bookDir = path.join(projectDir, book.name);
-    mkDir(bookDir);
+    const chapters = [];
     for await (const chapter of book.chapters) {
-      const result = await convertChapter(chapter, book, project, projectDir, ffprobePath);
-      if (result instanceof Error) {
-        throw result;
-      }
+      const result = await convertChapter(chapter, book, project, ffprobePath);
+      chapters.push(result);
     }
+    bkProject.books.push({ name: book.name, chapters });
   }
-  return projectDir;
+  return bkProject;
 }
 
 // Adapted from https://github.com/caffco/get-audio-duration:
