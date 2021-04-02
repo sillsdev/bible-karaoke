ba2e281125cdee3f43e22f9306b4213459c1cfe7
nate <7259@pm.me>
2021-04-02 12:16:47 +0700

    Change to use winston default logger.

--- public/cli/lib/commands/logger.integration.ts
+++ public/cli/lib/commands/logger.integration.ts
@@ -3,6 +3,7 @@ import tempy from 'tempy';
 import fs from 'fs';
 import path from 'path';
 import { prepareLogger } from './logger';
+import winston from 'winston';
 
 test('logger-removes-log-files', async (t) => {
   await tempy.directory.task((dir: string) => {
@@ -10,7 +11,8 @@ test('logger-removes-log-files', async (t) => {
     for (let i = 0; i < 10; i++) {
       fs.writeFileSync(path.join(dir, `${i}.txt`), 'test');
     }
-    prepareLogger(5, dir).end();
+    prepareLogger(5, dir);
+    winston.clear();
     const numberOfFiles = fs.readdirSync(dir).length;
     t.is(numberOfFiles, 5);
   });
--- public/cli/lib/commands/logger.ts
+++ public/cli/lib/commands/logger.ts
@@ -4,7 +4,14 @@ import { format as dateFormat } from 'date-fns';
 import winston from 'winston';
 import os from 'os';
 
-export function prepareLogger(numLogsToKeep = 10, pathToLogDir = ''): winston.Logger {
+// Importing isDev causes an error running tests as we don't run in an
+// electron environment. Ava sets the NODE_ENV to 'test'.
+let isDev = false;
+if (process.env.NODE_ENV !== 'test') {
+  isDev = require('electron-is-dev');
+}
+// Sets up the logger. Should be called when opening the app.
+export function prepareLogger(numLogsToKeep = 10, pathToLogDir = '') {
   // make sure the logging directory exists
   const homedir = os.homedir();
   if (pathToLogDir == '') {
@@ -40,7 +47,8 @@ export function prepareLogger(numLogsToKeep = 10, pathToLogDir = ''): winston.Lo
   const name = `${dateFormat(new Date(), 'yyyyMMdd_HHmmss')}.log`;
   const pathLogFile = join(pathToLogDir, name);
 
-  const logger = winston.createLogger({
+  // Configure winston default logger
+  winston.configure({
     level: 'info',
     format: winston.format.json(),
     defaultMeta: { service: 'user-service' },
@@ -51,17 +59,25 @@ export function prepareLogger(numLogsToKeep = 10, pathToLogDir = ''): winston.Lo
       }),
     ],
   });
-  logger.info('Logger Initialized');
 
   // If we're not in production then log to the `console` with the format:
   // `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
-  if (process.env.NODE_ENV !== 'production') {
-    logger.add(
+  if (isDev) {
+    winston.add(
       new winston.transports.Console({
-        format: winston.format.simple(),
+        format: winston.format.combine(
+          winston.format.colorize({
+            colors: {
+              error: 'red',
+              warn: 'yellow',
+              info: 'cyan',
+            },
+          }),
+          winston.format.simple()
+        ),
       })
     );
   }
 
-  return logger;
+  winston.info('Logger Initialized');
 }
